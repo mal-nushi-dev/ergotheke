ed121d8b99fb511f3fbaaa28c728078e9d0c3928
mal-nushi-dev <malnushi1999+dev@gmail.com>
2025-05-25 15:12:20 -0400

    Add About page with personal biography and metadata

---
+++ app/about/page.tsx
@@ -0,0 +1,44 @@
+export const metadata = {
+  title: 'About Me',
+  description: 'Learn about Mal Nushi',
+}
+
+export default function AboutPage() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">About Me</h1>
+      <p className="mb-4">
+        I am a dedicated software engineer specializing in data engineering and 
+        data systems at Ally Financial. In this role, I play a crucial part in 
+        supporting our business data needs, developing robust data solutions 
+        that drive strategic decisions and enhance operational efficiencies. 
+        My professional journey in data systems began after my academic studies 
+        at Wayne State University, where I honed my skills and fueled my passion 
+        for technology.
+        <br />
+        <br />
+        Beyond my specialization, I have a keen interest in a variety of 
+        technology domains. I am constantly engaged in writing, designing, 
+        and building new projects, driven by a desire to explore and innovate. 
+        My passion for tech is complemented by creative problem-solving, and I 
+        am always eager to expand my horizons and engage with new ideas in the 
+        tech world.
+        <br />
+        <br />
+        My personal story is marked by a significant chapter when I became a 
+        refugee from Kosovo, moving to Michigan in 1999 to escape the war. 
+        This experience has profoundly shaped my perspective and resilience. 
+        Since then, I was raised in Metro Detroit, a community that has played 
+        a pivotal role in both my personal and professional life.
+        <br />
+        <br />
+        Outside of work, my interests are diverse and reflect my curiosity 
+        about the world. I have a passion for photography, capturing moments 
+        and landscapes that tell stories. Other aveneus where I find joy and 
+        relaxation are: surf-skating, building legos, music, and soccer. I also 
+        cherish traveling, as it offers me opportunities to explore new cultures 
+        and environments, further broadening my worldview.
+      </p>
+    </section>
+  )
+}

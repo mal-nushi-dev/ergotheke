a8f8e6ece3aad6e9d0a7f0b8a9551f12a9cef854
Mal Nushi <malnushi1999+dev@gmail.com>
2025-05-27 23:31:25 -0400

    Add blog in footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -30,7 +30,8 @@ export default function Footer() {
             <p className="ml-2 h-7">github</p>
           </a>
         </li>
-                <li>
+
+        <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
@@ -39,6 +40,18 @@ export default function Footer() {
           >
             <ArrowIcon />
             <p className="ml-2 h-7">linkedin</p>
+          </a>
+        </li>
+
+                <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.kodikion.com"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">blog</p>
           </a>
         </li>
       </ul>

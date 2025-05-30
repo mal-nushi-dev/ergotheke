6c825f6a4f8d7edcd2085a1df5a9b64b82608a92
mal-nushi-dev <malnushi1999+dev@gmail.com>
2025-05-30 18:29:09 -0400

    Update under construction message for sidequest page

--- app/sidequests/[slug]/page.tsx
+++ app/sidequests/[slug]/page.tsx
@@ -2,13 +2,11 @@ export default function SidequestUnderConstruction() {
   return (
     <section className="flex flex-col items-center justify-center min-h-[40vh]">
       <h1 className="mb-4 text-2xl font-semibold tracking-tighter">
-        🚧 Under Construction
+        I am currently building this page.
       </h1>
       <p className="text-center text-gray-600 dark:text-neutral-100">
-        This sidequest page is under construction.<br />
-        Check back later!
+        Please do not rush me.<br />
       </p>
-      <p className="text-center text-[10px] text-gray-600 dark:text-neutral-100">Message from Mal: <i>Sorry</i></p>
     </section>
   )
 }

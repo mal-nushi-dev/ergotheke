13df7a84f6b2427922c19577962ed269e789e963
mal-nushi-dev <malnushi1999+dev@gmail.com>
2025-09-14 22:10:18 -0400

    Add overview section with personal insights for Penny Ocean Mist 22" Penny Board

--- app/sidequests/skating/skate_gear/pennyboard_ocean_mist/page.tsx
+++ app/sidequests/skating/skate_gear/pennyboard_ocean_mist/page.tsx
@@ -6,6 +6,18 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Penny Ocean Mist 22" Penny Board
       </h1>
+      <h2 className="font-semibold text-lg mb-2 tracking-tight">Overview</h2>
+      <p className="mb-4">
+        I honestly bought this board because a deal I saw on an Instagram ad was
+        too good to pass up on. This is the shortest board on my list by a long
+        shot and also the most narrow. The fact that there is no grip tape on it
+        makes it so difficult to ride. Some good things I would say about the
+        boards stock configuration is that trucks are responsive and while the
+        wheels are small, they are also soft enough to handle rough ground. I
+        have no issues so far with the bearings or pivot cups, though I have not
+        really ridden this board that much to wear anything out.
+      </p>
+      <br />
       <h2 className="font-semibold text-lg mb-2 tracking-tight">Board Specs</h2>
       <p className="mb-4">
         <ul>

72e575f54c2d905547dc42c74bbf0b5da4db83d8
mal-nushi-dev <malnushi1999+dev@gmail.com>
2025-09-14 22:28:42 -0400

    Add overview and specs sections for 187 Killer Pads 6-Pack Set

---
+++ app/sidequests/skating/skate_gear/killer_pads/page.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        187 Kiler Pads 6-Pack Set
+      </h1>
+      <h2 className="font-semibold text-lg mb-2 tracking-tight">Overview</h2>
+      <p className="mb-4">
+        I definitely recommend getting some safety gear for skating. These have
+        been a godsend at skateparks where it helps me get rid of my fear in a
+        lot of situations knowing I will be protected if I fall.
+      </p>
+      <br />
+      <h2 className="font-semibold text-lg mb-2 tracking-tight">Specs</h2>
+      <p className="mb-4">
+        <ul>
+          <li>
+            <strong>Contoured, form-fitting design</strong> that "cradles" the
+            knee" for better fit and movement.
+          </li>
+          <li>
+            <strong>Superior foam system</strong> to absorb hard impacts.
+          </li>
+          <li>
+            <strong>Durable outer shell/fabric</strong> that is made up of
+            ballistic nylon and industrial-weight stitching.
+          </li>
+        </ul>
+      </p>
+    </section>
+  );
+}
